15e63df5b2d518229f97a85cbf29016cbc95cb32
Josh Korol <jkorol3@gatech.edu>
2023-05-31 20:50:16 -0400

    Create basic infrastructure, add ignore
    set up skeleton

--- job_scraper.py
+++ job_scraper.py
@@ -2,9 +2,10 @@
 import requests
 from bs4 import BeautifulSoup
 """
-Copyright 2023 Josh Korol
+Copyright Josh Korol 2023
 All Rights Reserved
 
+Author: Josh Korol
 Created: May 31, 2023
 
 This script is a tool I use to scrape job listings across popular websites such as Linkedin, Wayup, Dice, and Indeed.
@@ -17,12 +18,9 @@ Usage:
 def scrape_job_listings():
     # TODO: handle 
 
-    url = ''
-
     # send get req
     response = requests.get(url)
     # create object to parse htmld
     soup = BeautifulSoup(response.content, 'html.parser')
     job_elements = soup.find_all('div', class_='job-listing')
 
-def query_dice():
