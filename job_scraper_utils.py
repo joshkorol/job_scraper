d14ba51f5b840587cd987c9cd1ee98f93e052942
Josh Korol <jkorol3@gatech.edu>
2023-06-01 19:44:55 -0400

    Update job_scraper_utils.py

--- job_scraper_utils.py
+++ job_scraper_utils.py
@@ -30,16 +30,27 @@ TODO add documentation
 def scrape_dice():
     # Create ChromeOptions object
     options = Options()
-    options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+    #options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
 
-    #options.add_experimental_option("detach", True)
+    options.add_experimental_option("detach", True)
     chrome_driver = "/usr/local/bin/chromedriver"
     driver = webdriver.Chrome(chrome_driver, service=Service(ChromeDriverManager().install()), options=options)
     driver.get('https://www.dice.com')
 
     # find releveant elements
-    input_job_element = driver.find_element(By.ID, "typeaheadInput").send_keys("JOB NAME")
+    # TODO: implement location, job name config
+    input_job_element = driver.find_element(By.ID, "typeaheadInput").send_keys("Software Engineering")
     input_location_element = driver.find_element(By.ID, "google-location-search").send_keys("Atlanta, GA, USA"+ Keys.ENTER)
+    
+    # - - - filter results - - -
+    # TODO: implement control about hybrid/remote
+    # exclude remote options
+    time.sleep(3)
+    exclude_remote_button = driver.find_element(By.XPATH, '//button[@aria-label="Filter Search Results by Exclude Remote"]')
+    exclude_remote_button.click()
 
+    work_from_home_button = driver.find_element(By.XPATH, '//button[@aria-label="Filter Search Results by Work From Home Available"]')
+    work_from_home_button.click()
+    print("Completed")
     # now search through query
     driver.quit()
